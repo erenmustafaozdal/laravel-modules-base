70f89d58403855079d3048b49146331b821b4cfa
erenMustafaOzdal <eren.060737@gmail.com>
2016-07-02 12:08:09 +0000

    permission

---
+++ src/Middlewares/Permission.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ErenMustafaOzdal\LaravelModulesBase\Middlewares;
+
+use Closure;
+use Sentinel;
+use Route;
+use Request;
+
+class Permission
+{
+    /**
+     * user routes
+     *
+     * @var array
+     */
+    private $userRoutes = [
+        'admin.user.show',
+        'admin.user.edit',
+        'admin.user.update',
+        'admin.user.changePassword',
+        'admin.user.permission',
+        'api.user.detail',
+        'api.user.fastEdit',
+        'api.user.update',
+        'api.user.destroyAvatar',
+        'api.user.avatarPhoto',
+    ];
+
+    /**
+     * user destroy routes
+     *
+     * @var array
+     */
+    private $userDestroyRoutes = [
+        'admin.user.destroy',
+        'api.user.destroy'
+    ];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $route = Route::currentRouteName();
+
+        // if user destroy route
+        if (
+            in_array($route, $this->userDestroyRoutes)
+            && Request::route('users')->id === Sentinel::getUser()->id
+        ) {
+            abort(403);
+        }
+
+        if (
+            (! in_array($route, $this->userRoutes)
+            || Request::route('users')->id !== Sentinel::getUser()->id)
+            && ! Sentinel::hasAccess( $route )
+        ) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
